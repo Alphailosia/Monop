51e44674aebcb6030b546e64b3f8400a3eedeca3
AllanDuvois <56909164+AllanDuvois@users.noreply.github.com>
2020-11-17 22:16:23 +0100

    Début d'ajout des cases chances #41  et caisse de communauté #42
    Ajout des cases chances et caisse de communauté.
    
    Fonctionnement non tester mais aucune regression au niveau du code constaté.
    
    /!\ Il faudra tout de même faire attention lors du merge que cela n'écrasse pas des changement effectuer sur d'autre branche, notamment dans Plateau.vue /!\

---
+++ monopoly/src/components/CaseCaisseCommunaute.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-card class="caisseCommunaute">
+        <div class="color-caisseCommunaute" :style="{backgroundColor:white}">
+        </div>
+        <v-card-title class="title-caisseCommunaute">
+            {{nom}}
+        </v-card-title>
+        <div class="label-caisseCommunaute">
+             {{label}}
+        </div>
+        <div class="image-caisseCommunaute">
+             <img src="../assets/images/coffre.png" /> 
+        </div>
+        <div class="description-caisseCommunaute">
+            Description : {{descritpion}} 
+        </div>
+    </v-card>
+</template>
+
+<script>
+export default {
+   props: {
+        nom: String,
+        label: String,
+        description: String,
+   } 
+}
+</script>
+
+<style scoped>
+
+.caisseCommunaute {
+width: 300px;
+height: 200px;
+border: solid 2px;
+border-radius: 5px;
+}
+
+.color-caisseCommunaute{
+height: 20%;
+}
+
+.title-caisseCommunaute {
+ text-align: center;
+ font-weight: bold;
+}
+
+.label-caisseCommunaute {
+text-align: center;
+justify-content: center;
+}
+
+.description-caisseCommunaute {
+text-align: center;
+width: 100%;
+margin-top: 80%;
+}
+
+</style>
---
+++ monopoly/src/components/CaseChance.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card>
+        {{nom}}, {{monopole}}
+    </v-card>
+</template>
+
+<script>
+export default {
+   props: {
+       monopole: String,
+       nom: String,
+   } 
+}
+</script>
+
+<style scoped>
+
+</style>
