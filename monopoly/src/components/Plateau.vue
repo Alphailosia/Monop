771c4cfd9ad18cbec49c406c66353ab78c160804
hassanbenab <44161562+hassanbenab@users.noreply.github.com>
2020-11-17 11:40:11 +0100

    Ajout des Cases services publiques #38

--- monopoly/src/components/Plateau.vue
+++ monopoly/src/components/Plateau.vue
@@ -6,30 +6,40 @@
     :nom="proprietes[0][1].nom" 
     :monopole="proprietes[0][0]"
     />
+
+    <CaseServicePublic 
+    :nom="services[0][1].nom" 
+    :monopole="services[0][0]"
+    />
   </div>
 </template>
 
 <script>
 import CasePropriete from "./CasePropriete";
+import CaseServicePublic  from "./CaseServicePublic";
 import Cartes_propriete_gares_services from "../Cartes_propriete_gares_services.json";
 
 export default {
   components: {
     CasePropriete,
+    CaseServicePublic
   },
   data: () => ({
     proprietes: [],
+    services: [],
     jsonProprietes: [],
   }),
-  mounted() {
+  created() {
     this.jsonProprietes = Cartes_propriete_gares_services;
     this.init();
   },
   methods: {
     init: function () {
         this.proprietes = this.jsonProprietes[0];
+        this.services = this.jsonProprietes[2];
         console.log(this.proprietes[0][1]);
         console.log(this.proprietes);
+
     },
   },
 };
