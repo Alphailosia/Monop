0ddbe9bfc3e202bbf285b0e7e453b1604c544268
Ewen Hanni <48356811+ewen-hanni@users.noreply.github.com>
2020-11-18 17:35:18 +0100

    finalisation forme taxe de luxe

--- monopoly/src/components/Taxe.vue
+++ monopoly/src/components/Taxe.vue
@@ -1,11 +1,17 @@
 <template>
   <v-card class="taxe">
-    <v-card-title class="title-propriete">
+    <v-card-title class="title-taxe">
       {{ nom }}
     </v-card-title>
+
+    <div class="image-taxes">
+      <img src="../assets/images/bague.png" alt="départ" />
+    </div>
     <v-card-title class="prix-taxe"> Prix : {{ prix }} euros </v-card-title>
   </v-card>
 </template>
+
+
 
 <script>
 export default {
@@ -17,23 +23,38 @@ export default {
 </script>
 
 <style scoped>
+
+
+.title-taxe {
+  width:200px;
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 15px;
+  font-size: 40px;
+}
+
+
+
 .taxe {
-  width: 200px;
+  width: 204px;
   height: 300px;
   border: solid 2px;
   border-radius: 5px;
 }
 
-.title-propriete {
-  text-align: center;
-  font-weight: bold;
-  margin-top: 20%;
-}
 
 .prix-taxe {
   text-align: center;
-  justify-content: end;
-  width: 100%;
-  margin-top: 80%;
+  justify-content: center;
+  margin-top: 170px;
 }
+
+.image-taxes {
+  height: 1px;
+  width: 100px;
+  margin-bottom: 40px;
+  margin-left: 25px;
+  margin-right: 75px;
+}
+
 </style>
