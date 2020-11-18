f431948de0638c89dffbaf3517b703e29d4da832
Sabrina <sabrina.mercuri@etu.unice.fr>
2020-11-18 16:14:33 +0100

    fix affichage case taxe #51

--- monopoly/src/components/Taxe.vue
+++ monopoly/src/components/Taxe.vue
@@ -1,39 +1,39 @@
 <template>
-    <v-card class="Taxe">
-        <div class="color-Taxe">
-        </div>
-        <v-card-title class="title-propriete">
-            {{nom}}
-        </v-card-title>
-
-        <v-card-title class="prix-taxe">
-            {{prix}}
-        </v-card-title>
-    </v-card>
+  <v-card class="taxe">
+    <v-card-title class="title-propriete">
+      {{ nom }}
+    </v-card-title>
+    <v-card-title class="prix-taxe"> Prix : {{ prix }} euros </v-card-title>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        props: {
-            nom: String,
-            prix: Number,
-        }
-    }
+export default {
+  props: {
+    nom: String,
+    prix: Number,
+  },
+};
 </script>
 
 <style scoped>
+.taxe {
+  width: 200px;
+  height: 300px;
+  border: solid 2px;
+  border-radius: 5px;
+}
 
+.title-propriete {
+  text-align: center;
+  font-weight: bold;
+  margin-top: 20%;
+}
 
-
-    .title-propriete {
-        text-align: center;
-        font-weight: bold;
-    }
-
-    .prix-taxe {
-        text-align: center;
-        justify-content: end;
-
-    }
-
+.prix-taxe {
+  text-align: center;
+  justify-content: end;
+  width: 100%;
+  margin-top: 80%;
+}
 </style>
