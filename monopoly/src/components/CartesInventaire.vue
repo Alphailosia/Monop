460349f276f63e49d0885e2d9b487cfc6e11ddff
Sabrina <sabrina.mercuri@etu.unice.fr>
2020-12-03 13:58:46 +0100

    début ajout affichage grand inventaire #53

---
+++ monopoly/src/components/CartesInventaire.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card
+    class="inventaire"
+    @click="affichageCartes()">
+    <div class="color" :style="{ backgroundColor: monopole }">
+    <v-card-title class="title-cartes">
+      {{ nom }}
+    </v-card-title>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import Cartes_propriete_gares_services from "../Cartes_propriete_gares_services.json";
+export default {
+  props: {
+    carte: Object,
+    monopole: String,
+    nom: String,
+  },
+};
+</script>
+
+<style scoped>
+.inventaire {
+  width: 50px;
+  height: 100px;
+  border: solid 2px;
+  border-radius: 5px;
+  position: relative;
+}
+
+.title-cartes {
+  text-align: center;
+  font-weight: bold;
+  word-break: break-word;
+  justify-content: center;
+}
+
+</style>
